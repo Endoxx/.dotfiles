da4bffd7c63f42e41caf35d47f4760b84bf9a05e
Endoxx <mae.newz@gmail.com>
2016-04-29 20:21:59 +0200

    Clean up commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+cd ~/
+
+
+# Update
+cd ~/.dotfiles
+git pull http://github.com/Endoxx/.dotfiles
+cd ~/
+
+# Backup & Remove existing
+cd ~/
+mkdir ~/.dotfiles/backup
+cp .zshrc ~/.dotfiles/backup
+cp .vimrc ~/.dotfiles/backup
+rm .zshrc
+rm .vimrc
+
+
+# Establishing links
+cd ~/.dotfiles
+ln -n .zshrc ~/
+ln -n .vimrc ~/
