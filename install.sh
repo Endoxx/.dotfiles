c46a6559e2144eb0cabacea25ce9682a9f42d9c6
Endoxx <mae.newz@gmail.com>
2016-05-03 18:22:42 +0200

    install.sh update

--- install.sh
+++ install.sh
@@ -1,16 +1,3 @@
-cd ~/
-
-
-# Update
-cd ~/.dotfiles
-git pull http://github.com/Endoxx/.dotfiles
-cd ~/
-# Create necessary directories
-mkdir ~/.vim
-mkdir ~/.vim/bundle
-cd ~/vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git
-
 # Backup & Remove existing
 cd ~/
 mkdir ~/.dotfiles/backup
@@ -24,6 +11,4 @@ rm .vimrc
 cd ~/.dotfiles
 ln -n .zshrc ~/
 ln -n .vimrc ~/
-
-ln -s .vim/ ~/
 
